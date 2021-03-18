e4b859d159b19c9c4a4fb6e8b1825261b59406ec
cesaranchundia <70555327+cesaranchundia@users.noreply.github.com>
2021-03-18 01:19:01 -0500

    Update and rename index.js to index.war

--- index.js
+++
@@ -1,4 +0,0 @@
-const core = require('@actions/core');
-const githubUrl = core.getInput('github-url');
-rawUrl = githubUrl.replace('//github.com/', '//raw.githubusercontent.com/').replace('/blob/', '/')
-console.log(`::set-output name=raw-url::${rawUrl}`)
